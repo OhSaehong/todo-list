9e1338fbdce12b474c9265faa7128d5019d32b2a
오세홍 <hongseoh@inha.edu>
2022-11-29 21:48:40 +0900

    221129_2148

---
+++ src/components/TodoItem.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class TodoItem extends Component {
+    render() {
+        const {text, checked, id, onToggle, onRemove} = this.props;
+        return <div className='todo-item' onClick={() => onToggle(id)}>
+            <div
+                className="remove"
+                onClick={(e) => {
+                e.stopPropagation();
+                onRemove(id);
+            }}>&times;</div>
+            <div className={`todo-text $(checked && 'checked'}`}>
+                <div>{text}</div>
+            </div>
+
+            {checked && (
+                <div className="check-mark"></div>
+            )}
+        </div>
+
+    };
+
+}
+
+export default TodoItem;
